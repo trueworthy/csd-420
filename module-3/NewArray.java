152faedbc3aeab310ed03e68f173d83bf5a3cd85
trueworthy <ltrueworthy@yahoo.com>
2025-04-05 10:33:37 -0600

    adding mod3 adn 4 folders

---
+++ module-3/NewArray.java
@@ -0,0 +1,4 @@
+// Lea Trueworthy
+// March 31, 2025
+// M3 Programming assignment
+// Description: Write a test program that removes duplicates from an ArrayList of 50 random numbers between 1 and 20 and should return a new list with only unique values.
---
+++ module-4/LinkedListTraversalTest.java
@@ -0,0 +1,4 @@
+// Lea Trueworthy
+// March 31, 2025
+// M4 Programming assignment
+// Description: Write a program that measures how fast a LinkedList can be traversed using an iterator compared to get(index).
