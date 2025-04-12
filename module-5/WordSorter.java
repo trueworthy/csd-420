87a5e1a3b109813fc9d9a4ca30df9527f2bb79d9
trueworthy <ltrueworthy@yahoo.com>
2025-04-12 07:15:37 -0600

    adding module 5 folder

---
+++ module-5/WordSorter.java
@@ -0,0 +1,5 @@
+// Lea Trueworthy
+// April 8, 2025
+// M5 Programming assignment
+// Description: Write a program that reads words from a file, removes duplicates, and sorts them in both ascending and descending orders.
+
