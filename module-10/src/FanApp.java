8629dc13a6b6847cce7ffd0554717702545e05e2
trueworthy <ltrueworthy@yahoo.com>
2025-05-10 08:23:45 -0600

    adding module 10

---
+++ module-10/src/FanApp.java
@@ -0,0 +1,5 @@
+// Lea Trueworthy
+// May 6, 2025
+// M10 Programming assignment
+// Description: Write a program that connects to a MySQL database (databasedb) using user ID student1 and password pass, and manages fan records in a table called fans. 
+// The GUI lets users enter an ID to display or update a fan's first name, last name, and favorite team. The program assumes the table exists but includes optional code to create and populate it for testing.
